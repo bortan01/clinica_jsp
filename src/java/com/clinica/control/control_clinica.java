604df5ad63b63e9e0f9c7224d8cc85b79d25d2c0
bortan01 <brmiranda009@gmail.com>
2017-11-18 22:38:52 -0600

    suave

---
+++ src/java/com/clinica/control/control_clinica.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.clinica.control;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean(name = "control_clinica")
+@SessionScoped
+public class control_clinica {
+    
+    
+}
---
+++ src/java/com/clinica/dao/pacienteDao.java
@@ -0,0 +1,7 @@
+package com.clinica.dao;
+
+import com.clinica.entidad.Paciente;
+
+public interface pacienteDao extends GenericDao<Paciente, Integer>{
+    
+}
